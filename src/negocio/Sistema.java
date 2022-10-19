22a09ef522f0c7cada7550eb5a7b9de499d34c14
Paula <paula.bonifazi.aquino@gmail.com>
2022-10-19 17:26:10 -0300

    Cabeceras mozo listas.
    Agregar javadoc.

--- src/negocio/Sistema.java
+++ src/negocio/Sistema.java
@@ -1,6 +1,7 @@
 package negocio;
 
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,17 +22,11 @@ import modelo.Sueldo;
  */
 public class Sistema {
     
-//    enum Estado{activo,
-//                deFranco,
-//                ausente}
-    
-    private String nombre;
-    private ArrayList<Mozo> mozos; 
-    private ArrayList<Producto> productos;
-    private ArrayList<Operario> operarios;
+    private HashMap<String,Mozo> mozos; 
+    private HashMap<Integer,Producto> productos;
+    private HashMap<String,Operario> operarios;
     private ArrayList<Mesa> mesas; 
     private static Sistema instance = null;
-    private Map <String, String> map = new HashMap<String, String>();
     private Sueldo sueldo;
     
     public Sistema getInstance(){
@@ -85,84 +80,32 @@ public class Sistema {
     
     public Operario login(String userName,String password)throws UserNameIncorrecto_Exception,ContrasenaIncorrecta_Exception {return null;}
     
-    /**
-     * Metodo para modificar algun atributo del operario en cuestion. <br>
-     * Pre: El campo NyA debe ser distinto de null y distinto de vacio. <br>
-     * Pre: El campo userName debe ser distinto de null y distinto de vacio. <br>
-     * Pre: El campo password debe ser distinto de null y distinto de vacio. <br>
-     * Pre: El campo activo debe ser distinto de null y distinto de vacio. <br>
-     * @param operario Representa al operario a modificar. <br>
-     * @param NyA
-     * @param userName
-     * @param password
-     * @param activo
-     * @throws UserNameRepetido_Exception si el nombre de usuario ingresado esta asociado a otra cuenta.
-     */
-    public void modificaOperario (Operario operario, String NyA, String userName, String password, boolean activo) throws UserNameRepetido_Exception {}
+ 
+    public void modificaOperarioNyA (String userName, String NyA){}
     
-    /**
-     * Metodo para eliminar un operario del sistema. <br>
-     * Pre: El operario debe ser distinto de null o vacio. <br>
-     * Post: Se elimina el operario del sistema. <br>
-     * @param operario Representa a un operario a eliminar. <br>
-     * @throws NoExisteOperario_Exception si no esta registrado el operario en el sistema.
-     */
-    public void eliminaOperario (Operario operario)throws NoExisteOperario_Exception{}
+    public void modificaOperarioUsername (String userName, String newUserName) throws UserNameRepetido_Exception {}
     
-    /**
-     * Metodo para agregar un mozo al sistema.<br>
-     * Pre: El mozo debe ser distinto de null o vacio. <br>
-     * Post: Se agrega el mozo al sistema. <br>
-     * @param mozo Representa a un mozo a agregar.
-     */
-    public void agregaMozo(Mozo mozo){}
+    public void modificaOperarioPassword (String userName, String password){}
     
+    public void modificaOperarioEstado (String userName, boolean activo){}
     
-    //es redundante??
+
+
+    public void eliminaOperario (String userName)throws NoExisteOperario_Exception{}
     
-    /**
-     * Metodo para modificar los datos de un mozo. <br>
-     * Pre: El mozo debe ser distinto de null o vacio. <br>
-     * Post: Se modificaron los datos del mozo. <br>
-     * @param mozo Representa a un mozo a modificar.
-     */
-    public void modificaMozo(Mozo mozo){}
+
+    public void agregaMozo(String NyA, GregorianCalendar fecha, int cantHijos, Estado estado){}
+
+
+    public void modificaMozoNyA(String NyA, String newNyA){}
+
+
+    public void modificaMozoCantHijos(String NyA, int cantHijos){}
     
-    /**
-     * Metodo para modificar el nombre y el apellido de un mozo. <br>
-     * Pre: El NyA debe ser distinto de null o vacio. <br>
-     * Post: Se modificaron NyA del mozo. <br>
-     * @param mozo Representa a un mozo a modificar.
-     * @param NyA Nombre y Apellido a modificar.
-     */
-    public void modificaMozo(Mozo mozo, String NyA){}
+
+    public void modificaMozoEstado(String NyA, Estado estado){}
     
-    /**
-     * Metodo para modificar la cantidad de hijos de un mozo. <br>
-     * Pre: La cantidad de hijos debe ser mayor a cero o vacio. <br>
-     * Post: Se modificaron la cantidad de hijos del mozo. <br>
-     * @param mozo Representa a un mozo a modificar.
-     * @param cantHijos Cantidad de hijos a modificar.
-     */
-    public void modificaMozo(Mozo mozo, int cantHijos){}
-    
-    /**
-     * Metodo para modificar el estado de un mozo. <br>
-     * Pre: El estado debe ser distinto de null o vacio. <br>
-     * Post: Se modifico el estado del mozo. <br>
-     * @param mozo Representa a un mozo a modificar.
-     * @param estado Estado a modificar.
-     */
-    public void modificaMozo(Mozo mozo, Estado estado){}
-    
-    /**
-     * Metodo para eliminar un mozo del sistema. <br>
-     * Pre: El mozo debe ser distinto de null o vacio. <br>
-     * Post: Se elimina el mozo del sistema. <br>
-     * @param mozo Representa a un mozo a eliminar. <br>
-     * @throws NoExisteMozo_Exception si no esta registrado el mozo en el sistema.
-     */
-    public void eliminaMozo(Mozo mozo) throws NoExisteMozo_Exception {}
+    public void eliminaMozo(String NyA) throws NoExisteMozo_Exception {}
     
     
 }
