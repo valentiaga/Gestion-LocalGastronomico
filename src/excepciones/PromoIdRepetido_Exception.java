f16bc0cb2928ac5361a3e054b504b8c245366bd2
valentiaga <valenreale123@gmail.com>
2022-10-20 19:05:23 -0300

    modelado y javadoc

---
+++ src/excepciones/PromoIdRepetido_Exception.java
@@ -0,0 +1,9 @@
+package excepciones;
+
+public class PromoIdRepetido_Exception extends Throwable {
+
+    public PromoIdRepetido_Exception(String message) {
+        // TODO Implement this method
+        super(message);
+    }
+}
--- src/modelo/Mozo.java
+++ src/modelo/Mozo.java
@@ -8,5 +8,20 @@ public class Mozo
 	private String NyA;
 	private GregorianCalendar fecha;
 	private int cantHijos;
-	
+
+
+    /**
+     * Constructor de cada mozo. <br>
+     * Pre: el mozo debe ser mayor a 18 años. <br>
+     * Pre: la cantidad de hijos debe ser >= 0. <br>
+     * @param NyA perteneciente al nombre y apellido del mozo. <br>
+     * @param cantHijos cantidad de hijos que tiene el mozo. <br>
+     */
+    public Mozo(String NyA, int cantHijos) {
+        this.estado = Enumerados.estadoMozo.ACTIVO;
+        this.NyA = NyA;
+        this.fecha = new GregorianCalendar(); //setear fecha actual
+        this.cantHijos = cantHijos;
+    }
+
 }
