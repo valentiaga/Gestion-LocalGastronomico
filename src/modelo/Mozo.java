f58066582fff6066fb97ebf6d700dd4e861e966d
valentiaga <valenreale123@gmail.com>
2022-10-20 17:04:26 -0300

    probando

--- src/modelo/Mozo.java
+++ src/modelo/Mozo.java
@@ -4,11 +4,9 @@ import java.util.GregorianCalendar;
 
 public class Mozo
 {
-
 	private Enumerados.estadoMozo estado;
 	private String NyA;
 	private GregorianCalendar fecha;
 	private int cantHijos;
-	
 	
 }
