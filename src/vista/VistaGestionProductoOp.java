fc8b9bed629c44d9667794a148aaf082b59de85e
valentiaga <valenreale123@gmail.com>
2022-11-11 19:15:03 -0300

    mas cosas

--- src/vista/VistaGestionProductoOp.java
+++ src/vista/VistaGestionProductoOp.java
@@ -1,20 +1,41 @@
 package vista;
 
 import javax.swing.JPanel;
+import java.awt.BorderLayout;
 import javax.swing.JButton;
+import javax.swing.border.TitledBorder;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
-public class VistaGestionProductoOp extends JPanel {
+public class VistaGestionProductoOp extends JPanel implements IVistaGestionProductoOp{
 
 	/**
 	 * Create the panel.
 	 */
 	public VistaGestionProductoOp() {
+		setBorder(new TitledBorder(null, "Gestion Productos Operario", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		
-		JPanel panel = new JPanel();
-		add(panel);
+		JPanel panel_1 = new JPanel();
 		
-		JButton btnNewButton = new JButton("Modifica Producto");
-		add(btnNewButton);
+		JButton btnModificaProducto = new JButton("Modifica Producto");
+		panel_1.add(btnModificaProducto);
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 448, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(123)
+					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(132, Short.MAX_VALUE))
+		);
+		setLayout(groupLayout);
 
 	}
 
