6aeb099a9364e707fd7b914253427d2b81edf045
valentiaga <valenreale123@gmail.com>
2022-11-12 20:01:18 -0300

    exc

---
+++ src/excepciones/StockInsuficiente_Exception.java
@@ -0,0 +1,10 @@
+package excepciones;
+
+public class StockInsuficiente_Exception extends Exception {
+
+	public StockInsuficiente_Exception(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
